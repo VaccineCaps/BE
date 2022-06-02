9095f4464cee29f64ac09bd58be6c384e52bb9a5
Irfan Cahyo <cahyoariawan21@gmail.com>
2022-06-02 18:47:10 +0700

    🧬 Create Jwt Token for User Auth

---
+++ helper/parse_jwt.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"BE/model"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func ClaimJwt(tokenString string) (model.User, error) {
+	user := model.User{}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("JWT_KEY")), nil
+	})
+	if err != nil {
+		return user, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userClaimMarshaled, _ := json.Marshal(claims)
+		if err := json.Unmarshal(userClaimMarshaled, &user); err != nil {
+			return user, err
+		}
+	}
+	return user, nil
+}
