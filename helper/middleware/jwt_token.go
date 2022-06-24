3aa4c40a2a798ac4ea7f326c0e8f71da2c7b11d1
Irfan Cahyo <cahyoariawan21@gmail.com>
2022-06-24 19:35:23 +0700

    change duration jwt token expired

--- helper/middleware/jwt_token.go
+++ helper/middleware/jwt_token.go
@@ -8,7 +8,7 @@ import (
 
 func CreateToken(id int, roleID int, email, secret string) (string, error) {
 	claims := jwt.MapClaims{}
-	claims["exp"] = time.Now().Add((30 * time.Minute)).Unix()
+	claims["exp"] = time.Now().Add((120 * time.Minute)).Unix()
 	claims["iat"] = time.Now().Unix()
 	claims["nbf"] = time.Now().Add((25 * time.Second)).Unix()
 	claims["role_id"] = roleID
