09147dc408ff2f2bf3e1026c40e7387d481bb2e9
Irfan Cahyo <cahyoariawan21@gmail.com>
2022-07-07 15:29:30 +0700

    add image profile on user model

--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ type User struct {
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required"`
 	Password string `json:"-" validate:"required"`
+	Image    string `json:"image"`
 	RoleId   int    `gorm:"not null; default:2" json:"role_id"`
 	Role     Role   `gorm:"ForeignKey:RoleId;references:ID;null" json:"-"`
 }
