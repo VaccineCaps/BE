9b5927f163f5e5e4b7b48d54be108d8b76a7d7b3
unknown <azkazainur@gmail.com>
2022-07-13 12:26:10 +0700

    update hospital, news, advertise

--- model/hospitals.go
+++ model/hospitals.go
@@ -4,6 +4,7 @@ type Hospitals struct {
 	ID   			int    		`json:"id" gorm:"PrimaryKey;AUTO_INCREMENT;column:id"`
 	Name 			string 		`json:"name"`
 	Address 		string 		`json:"address"`
+	Email 			string 		`json:"email"`
 	UserId			int 		`gorm:"not null" json:"user_id"`
 	CitiesId   		int    		`gorm:"not null" json:"cities_id"`
 	User     		User	 	`gorm:"ForeignKey:UserId;references:ID;null" json:"-"`
